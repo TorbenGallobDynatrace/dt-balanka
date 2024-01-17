c5485268f915257d5269203a7d59db916b74b5f4
Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
2024-01-17 09:24:47 +0100

    Setup eslint

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,41 +1,41 @@
 import '@mantine/core/styles.css';
-import './App.css'
+import './App.css';
 import { AppShell, Burger, Group, Image, Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 
-function App() {
+function App () {
   const [opened, { toggle }] = useDisclosure();
 
   return (
     <AppShell
-    header={{ height: 64 }}
-    navbar={{
-      width: 300,
-      breakpoint: 'sm',
-      collapsed: { mobile: !opened },
-    }}
-    padding="md"
-  >
-    <AppShell.Header>
-      <Burger
-        opened={opened}
-        onClick={toggle}
-        hiddenFrom="sm"
-        size="sm"
-      />
-      <Group justify='flex-start' p={8} gap={16}>
-        <Image src="dynatrace.svg"  height={48} />
-        <Title size={18} order={1}>Dynatrace Balanka</Title>
-      </Group>
-    </AppShell.Header>
+      header={{ height: 64 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened }
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger
+          opened={opened}
+          onClick={toggle}
+          hiddenFrom="sm"
+          size="sm"
+        />
+        <Group justify='flex-start' p={8} gap={16}>
+          <Image src="dynatrace.svg" height={48} />
+          <Title size={18} order={1}>Dynatrace Balanka</Title>
+        </Group>
+      </AppShell.Header>
 
-    <AppShell.Navbar p="md">
-      
-    </AppShell.Navbar>
+      <AppShell.Navbar p="md">
 
-    <AppShell.Main>Main</AppShell.Main>
-  </AppShell>
-  )
+      </AppShell.Navbar>
+
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  );
 }
 
-export default App
+export default App;
